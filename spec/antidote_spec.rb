1d5c00b0fc5b0b07519ace149d5f6f592fed5168
Mathias Jean Johansen <mathias@mjj.io>
2014-11-07 19:03:26 +0100

    Update spec with more examples.

--- spec/antidote_spec.rb
+++ spec/antidote_spec.rb
@@ -2,13 +2,15 @@ require 'spec_helper'
 
 describe Antidote do
   describe Foo do
+    let(:foo) { Foo.new }
+
     describe "#bar" do
       it "raises an error when the method is given the wrong type" do
-        expect { Foo.new.bar(5.4) }.to raise_error
+        expect { foo.bar(5.4) }.to raise_error
       end
 
       it "returns x when given the right argument" do
-        expect(Foo.new.bar(5)).to eq 5
+        expect(foo.bar(5)).to eq 5
       end
     end
 
@@ -19,6 +21,18 @@ describe Antidote do
 
       it "returns [name, y] when given the right arguments" do
         expect(Foo.quux(6.66, "Hello")).to eq ["Hello", 6.66]
+      end
+    end
+
+    describe "#baz" do
+      let(:custom_type) { CustomType.new(1, 2) }
+
+      it "raises an error when the method is given the wrong types" do
+        expect { foo.baz(5.5) }.to raise_error
+      end
+
+      it "returns x.a + x.b when given the right arguments" do
+        expect(foo.baz(custom_type)).to eq 3
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,15 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require './lib/antidote'
 
+class CustomType
+  attr_accessor :a, :b
+
+  def initialize(a, b)
+    @a = a
+    @b = b
+  end
+end
+
 class Foo
   include Antidote
 
@@ -9,6 +18,11 @@ class Foo
     x
   end
   annotate("bar", x: Fixnum)
+
+  def baz(z)
+    z.a + z.b
+  end
+  annotate("baz", z: CustomType)
 
   def self.quux(y, name)
     [name, y]
