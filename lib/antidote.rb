965bb864e679208ba4a14000596477e2ac0aa88d
Mathias Jean Johansen <mathias@mjj.io>
2015-02-14 18:51:08 +0100

    Update syntax for annotations.

--- lib/antidote.rb
+++ lib/antidote.rb
@@ -1,90 +1,85 @@
-require_relative 'antidote/type_checker'
 require_relative "antidote/variable_type_error"
 require_relative "antidote/return_type_error"
 
 module Antidote
-  module ClassMethods
-    @@type_constraints = Hash.new { |h, k| h[k] = [] }
+  class << self
+    def included(klass)
+      klass.extend(ClassMethods)
+    end
+  end
 
-    def annotate(type_constraints, method_name)
-      add_type_constraints(method_name, type_constraints)
-      define_type_check_for_instance_method(method_name)
+  module ClassMethods
+    @@type_constraints = Hash.new { |h, k| h[k] = [] } 
+
+    def annotate(*variables, return_type, &method_name)
+      method_name = method_name.call.to_s
+      add_type_constraints(method_name, variables, return_type)
+      redefine_instance_method(method_name)
     end
 
-    def annotate_class_method(type_constraints, method_name)
-      add_type_constraints("self.#{method_name}", type_constraints)
-      define_type_check_for_class_method("self.#{method_name}")
+    def annotate_class_method(*variables, return_type, &method_name)
+      method_name = "self.#{method_name.call.to_s}"
+      add_type_constraints(method_name, variables, return_type)
+      redefine_class_method(method_name)
     end
 
     private
 
-    def add_type_constraints(method_name, type_constraints)
-      type_constraints.first.each do |variable|
-        @@type_constraints[method_name] << variable
-      end
-
-      @@type_constraints[method_name] << {__returns: type_constraints.last}
+    def add_type_constraints(method_name, variables, return_type)
+      variables.each { |variable| @@type_constraints[method_name] << variable }
+      @@type_constraints[method_name] << {__returns: return_type}
     end
 
-    def define_type_check_for_class_method(method_name)
+    def redefine_class_method(method_name)
       method_name = method_name.split(".").last
-      new_name_for_old_function = "#{method_name}_old".to_sym
-      self.singleton_class.send(:alias_method, new_name_for_old_function,
-                                method_name)
+      new_name_for_old_method = "#{method_name}_old".to_sym
+      self.singleton_class.send(:alias_method, new_name_for_old_method, method_name)
 
       define_singleton_method(method_name) do |*args|
-        args.each_with_index do |argument_value, index|
-          actual_type   = argument_value.class
-          expected_type =
-            @@type_constraints["self.#{method_name}"].first.values[index]
-          variable_name =
-            @@type_constraints["self.#{method_name}"].first.keys[index]
-          @@return_type = @@type_constraints["self.#{method_name}"].last.values.first
+        args.each_with_index do |argument, index|
+          class_method = "self.#{method_name}"
+          actual = argument.class
+          expected = @@type_constraints[class_method][index]
+          parameters = self.method(new_name_for_old_method).parameters.map(&:last)
+          variable_name = parameters[index]
+          @__return_type = @@type_constraints[class_method].last[:__returns]
 
-          unless actual_type == expected_type
-            raise Antidote::VariableTypeError,
-              [actual_type, expected_type, variable_name, "self.#{method_name}"]
+          unless actual == expected
+            raise VariableTypeError, [actual, expected, variable_name, class_method]
           end
         end
 
-        if (klass = send(new_name_for_old_function, *args).class) != @@return_type
-          raise Antidote::ReturnTypeError, [klass, @@return_type, "self.#{method_name}"]
+        if (klass = self.send(new_name_for_old_method, *args).class) != @__return_type 
+          raise ReturnTypeError, [klass, @__return_type, class_method]
         else
-          self.send(new_name_for_old_function, *args)
+          self.send(new_name_for_old_method, *args)
         end
       end
     end
 
-    def define_type_check_for_instance_method(method_name)
-      new_name_for_old_function = "#{method_name}_old".to_sym
-      alias_method(new_name_for_old_function, method_name)
+    def redefine_instance_method(method_name)
+      new_name_for_old_method = "#{method_name}_old".to_sym
+      alias_method(new_name_for_old_method, method_name)
 
       define_method(method_name) do |*args|
-        args.each_with_index do |argument_value, index|
-          actual_type   = argument_value.class
-          expected_type = @@type_constraints[method_name].first.values[index]
-          variable_name = @@type_constraints[method_name].first.keys[index]
+        args.each_with_index do |argument, index|
+          actual = argument.class
+          expected = @@type_constraints[method_name][index]
+          parameters = self.method(new_name_for_old_method).parameters.map(&:last)
+          variable_name = parameters[index]
+          @__return_type = @@type_constraints[method_name].last[:__returns]
 
-          @@return_type = @@type_constraints[method_name].last.values.first
-
-          unless actual_type == expected_type
-            raise Antidote::VariableTypeError,
-              [actual_type, expected_type, variable_name, method_name]
+          unless actual == expected
+            raise VariableTypeError, [actual, expected, variable_name, method_name]
           end
         end
 
-        if (klass = send(new_name_for_old_function, *args).class) != @@return_type
-          raise Antidote::ReturnTypeError, [klass, @@return_type, method_name]
+        if (klass = self.send(new_name_for_old_method, *args).class) != @__return_type 
+          raise ReturnTypeError, [klass, @__return_type, method_name]
         else
-          send(new_name_for_old_function, *args)
+          self.send(new_name_for_old_method, *args)
         end
       end
-    end
-  end
-
-  class << self
-    def included(klass)
-      klass.extend(ClassMethods)
     end
   end
 end
